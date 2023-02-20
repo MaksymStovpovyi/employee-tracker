4a41e94290949a91f8269525abca4dce4cee6407
maxSt <stovpovymax@gmail.com>
2023-02-20 12:40:47 -0500

    added schema.sql

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+// created packages
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
 
+// database connection
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '_data747',
+      database: '***'
+    },
+    console.log(`Connected to the *** database.`)
+);
