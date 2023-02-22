d7675cc0812b2badc4f01c42a0365992f89ae379
maxSt <stovpovymax@gmail.com>
2023-02-21 21:31:27 -0500

    prompt v2.0

--- index.js
+++ index.js
@@ -1,56 +1,37 @@
-// created packages
+// Importing dependencies
 const inquirer = require('inquirer');
-const cTable = require('console.table');
 const mysql = require('mysql2');
-const fs = require('fs');
+const cTable = require('console.table');
 
-// database connection
+
+// Creating connection to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: '_data747',
-      database: 'employee_db'
+        host: 'localhost',
+        user: 'root',
+        password: '_data747',
+        database: 'employee_db'
     },
-    console.log('')
-);
+    console.log(`Connected to the employees database.`)
 
-inquirer
-    .prompt([
-        {
+
+);
+// start function
+function  start() {
+    inquirer.prompt([{
         type: 'list',
+        name: 'menu',
         message: 'What would you like to do?',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
-        name: 'likeToDo',
-        }
-    ])
-    .then((response) => {
-        const { likeToDo } = response;
-
-        if (likeToDo === 'view all departments') {
-            viewAll('department');        
-        } else if (likeToDo === 'view all roles') {
-            viewAll('role');        
-        } else if (likeToDo === 'view all employees') {
-            viewAll('employee');        
-        } else if (likeToDo === 'add a department') {
-            add('department');
-        } else if (likeToDo === 'add a role') {
-
-        } else if (likeToDo === 'add an employee') {
-
-        } else if (likeToDo === 'update an employee role') {
-
-        }
-    }
-);
-
-function viewAll(table) {
-    console.log('');
-    db.query(`SELECT * FROM ${table}`,(err, results) => {
-        console.table(results);
-    });
-}
-
-
-
+        choices: 
+            [
+            'view all departments', 
+            'view all roles', 
+            'view all employees', 
+            'add a department', 
+            'add a role', 
+            'add an employee', 
+            'update an employee role',
+            'exit'
+            ]
+    }]).then(() => {})}
+        
