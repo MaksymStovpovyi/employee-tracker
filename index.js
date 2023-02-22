af494ea9acdb3fb8160b01e2fcba7a78e6fc6ae8
maxSt <stovpovymax@gmail.com>
2023-02-21 21:59:13 -0500

    view function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Importing dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
 
-// Creating connection to database
+// connection to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -13,25 +12,54 @@ const db = mysql.createConnection(
         database: 'employee_db'
     },
     console.log(`Connected to the employees database.`)
-
-
 );
+
 // start function
 function  start() {
     inquirer.prompt([{
-        type: 'list',
-        name: 'menu',
-        message: 'What would you like to do?',
-        choices: 
-            [
-            'view all departments', 
-            'view all roles', 
-            'view all employees', 
-            'add a department', 
-            'add a role', 
-            'add an employee', 
-            'update an employee role',
-            'exit'
+            type: 'list',
+            name: 'likeToDo',
+            message: 'What would you like to do?',
+            choices: [
+                '1. view all departments', 
+                '2. view all roles', 
+                '3. view all employees', 
+                '4. add a department', 
+                '5. add a role', 
+                '6. add an employee', 
+                '7. update an employee role',
+                '8. exit'
             ]
-    }]).then(() => {})}
-        
+        }]).then((res) => {
+            switch (res.likeToDo) {
+                case '1. view all departments':
+                    view('department');
+                    break;
+                case '2. view all roles':
+                    view('role');
+                    break;
+                case '3. view all employees':
+                    view('employee');
+                    break;
+                case '4. add a department':
+                    break;
+                case '5. add a role':
+                    break;
+                case '6. add an employee':
+                    break;
+                case '8. exit':
+                    db.end();
+                    break;
+            }
+        })
+}
+start();
+
+// view function
+function view(para) {
+    db.query(`SELECT * FROM ${para}`, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+        start();
+    })
+}
